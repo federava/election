92f781d73f9e94f03a428717fcba6c2e7865c961
federava <federrava@gmail.com>
2022-01-30 02:29:49 +0100

    Major changes

---
+++ contracts/Election.sol
@@ -0,0 +1,27 @@
+pragma solidity 0.5.16;
+
+contract Election {
+    
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping (uint => Candidate) public candidates;
+
+    uint public candidatesCount;
+
+    // Constructor
+    constructor () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private { // because we want only the SC to call this function, not anyone from outside
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+}
+
